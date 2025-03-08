27e1182d31edc4088723836c0504ef274dace6a9
kopsus <emonsetio01@gmail.com>
2025-03-08 14:20:29 +0700

    update : csr to ssr in product page

--- src/app/(private)/product/page.tsx
+++ src/app/(private)/product/page.tsx
@@ -1,37 +1,19 @@
-"use client";
-
-import { storeDialogProduct } from "@/api/product/store";
 import PageHeader from "@/components/page-header";
 import DialogDelete from "@/components/dialog/product/DialogDelete";
 import DialogMutation from "@/components/dialog/product/DialogMutation";
 import { ColumnsProduct } from "@/components/tables/product/Columns";
 import { DataTable } from "@/components/tables/product/DataTable";
 import { dataProduct } from "@/data/product";
-import { useSetAtom } from "jotai";
-import React from "react";
 
-const Product = () => {
-  const setDialog = useSetAtom(storeDialogProduct);
-
+const page = () => {
   return (
     <>
       <PageHeader title="Products" />
-      <DataTable
-        onClick={() => {
-          setDialog({
-            type: "CREATE",
-            show: true,
-            data: null,
-          });
-        }}
-        title="Product"
-        data={dataProduct}
-        columns={ColumnsProduct}
-      />
+      <DataTable title="Product" data={dataProduct} columns={ColumnsProduct} />
       <DialogMutation />
       <DialogDelete />
     </>
   );
 };
 
-export default Product;
+export default page;
