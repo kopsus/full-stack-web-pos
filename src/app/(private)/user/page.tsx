784c49644a9f9b9602791399fd82768dc2e32ed9
kopsus <emonsetio01@gmail.com>
2025-03-08 14:23:58 +0700

    update : csr to ssr in user page

--- src/app/(private)/user/page.tsx
+++ src/app/(private)/user/page.tsx
@@ -1,37 +1,19 @@
-"use client";
-
-import { storeDialogUser } from "@/api/user/store";
 import PageHeader from "@/components/page-header";
 import DialogDelete from "@/components/dialog/user/DialogDelete";
 import DialogMutation from "@/components/dialog/user/DialogMutation";
 import { ColumnsUser } from "@/components/tables/user/Columns";
 import { DataTable } from "@/components/tables/user/DataTable";
 import { dataUser } from "@/data/user";
-import { useSetAtom } from "jotai";
-import React from "react";
 
-const User = () => {
-  const setDialog = useSetAtom(storeDialogUser);
-
+const page = () => {
   return (
     <>
       <PageHeader title="User" />
-      <DataTable
-        onClick={() => {
-          setDialog({
-            type: "CREATE",
-            show: true,
-            data: null,
-          });
-        }}
-        title="User"
-        data={dataUser}
-        columns={ColumnsUser}
-      />
+      <DataTable title="User" data={dataUser} columns={ColumnsUser} />
       <DialogMutation />
       <DialogDelete />
     </>
   );
 };
 
-export default User;
+export default page;
