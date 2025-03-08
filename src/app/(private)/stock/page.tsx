e4634f8367a875da57482246d5e8b2588b86fd5b
kopsus <emonsetio01@gmail.com>
2025-03-08 14:21:41 +0700

    update : csr to ssr in stock page

--- src/app/(private)/stock/page.tsx
+++ src/app/(private)/stock/page.tsx
@@ -1,34 +1,17 @@
-"use client";
-
-import { storeDialogStock } from "@/api/stock/store";
 import PageHeader from "@/components/page-header";
 import { DataTable } from "@/components/tables/stock/DataTable";
 import { dataStock } from "@/data/stock";
-import { useSetAtom } from "jotai";
-import React from "react";
 import DialogMutation from "@/components/dialog/stock/DialogMutation";
 import { ColumnsStock } from "@/components/tables/stock/Columns";
 
-const Stock = () => {
-  const setDialog = useSetAtom(storeDialogStock);
+const page = () => {
   return (
     <>
       <PageHeader title="Stock" />
-      <DataTable
-        onClick={() => {
-          setDialog({
-            type: "CREATE",
-            show: true,
-            data: null,
-          });
-        }}
-        title="Stock"
-        data={dataStock}
-        columns={ColumnsStock}
-      />
+      <DataTable title="Stock" data={dataStock} columns={ColumnsStock} />
       <DialogMutation />
     </>
   );
 };
 
-export default Stock;
+export default page;
