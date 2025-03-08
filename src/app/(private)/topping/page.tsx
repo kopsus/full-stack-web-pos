8cb7f188c7ef552e8076a49003e91a410c612204
kopsus <emonsetio01@gmail.com>
2025-03-08 14:22:50 +0700

    update : csr to ssr in topping page

--- src/app/(private)/topping/page.tsx
+++ src/app/(private)/topping/page.tsx
@@ -1,37 +1,19 @@
-"use client";
-
-import { storeDialogTopping } from "@/api/topping/store";
 import PageHeader from "@/components/page-header";
 import DialogDelete from "@/components/dialog/topping/DialogDelete";
 import DialogMutation from "@/components/dialog/topping/DialogMutation";
 import { ColumnsTopping } from "@/components/tables/topping/Columns";
 import { DataTable } from "@/components/tables/topping/DataTable";
 import { dataTopping } from "@/data/topping";
-import { useSetAtom } from "jotai";
-import React from "react";
 
-const Topping = () => {
-  const setDialog = useSetAtom(storeDialogTopping);
-
+const page = () => {
   return (
     <>
       <PageHeader title="Topping" />
-      <DataTable
-        onClick={() => {
-          setDialog({
-            type: "CREATE",
-            show: true,
-            data: null,
-          });
-        }}
-        title="Topping"
-        data={dataTopping}
-        columns={ColumnsTopping}
-      />
+      <DataTable title="Topping" data={dataTopping} columns={ColumnsTopping} />
       <DialogMutation />
       <DialogDelete />
     </>
   );
 };
 
-export default Topping;
+export default page;
