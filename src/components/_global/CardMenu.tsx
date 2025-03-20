7a206770b49f0e6f443c608cfb746b694e76163c
kopsus <emonsetio01@gmail.com>
2025-03-20 22:20:09 +0700

    feat : image menu

--- src/components/_global/CardMenu.tsx
+++ src/components/_global/CardMenu.tsx
@@ -14,11 +14,13 @@ interface ICardMenu {
 }
 
 const CardMenu = ({ id, name, price, image, addToCart }: ICardMenu) => {
+  console.log("Data image", image);
+
   return (
     <Card className="rounded-xl overflow-hidden">
       <div className="w-full h-36 lg:h-40 overflow-hidden">
         <Image
-          src={image}
+          src={`/uploads/${image}`}
           alt={name}
           width={0}
           height={0}
