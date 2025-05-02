d5642205f7358f58a15d0c6bd6455e786720499a
kopsus <emonsetio01@gmail.com>
2025-05-02 23:06:09 +0700

    feat : history by shift

--- src/app/(private)/history/page.tsx
+++ src/app/(private)/history/page.tsx
@@ -3,6 +3,10 @@ import { ColumnsHistory } from "@/components/tables/history/Columns";
 import PageHeader from "@/components/page-header";
 import React from "react";
 import prisma from "@/lib/prisma";
+import { filterTransactionsByShift } from "@/utils/filterTransactionByShift";
+import { cookies } from "next/headers";
+import { decrypt } from "@/lib/actions/session";
+import { getActiveShift } from "@/utils/getActiveShift";
 
 const page = async () => {
   const transactions = await prisma.transaksi.findMany({
@@ -30,10 +34,30 @@ const page = async () => {
     },
   });
 
+  const cookie = (await cookies()).get("session")?.value;
+  const session = await decrypt(cookie);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: session?.id as string,
+    },
+  });
+
+  const activeShift = await getActiveShift(user?.id || "");
+
+  const filteredTransaction = filterTransactionsByShift(
+    transactions,
+    activeShift as any
+  );
+
   return (
     <>
       <PageHeader title="History" />
-      <DataTable title="History" data={transactions} columns={ColumnsHistory} />
+      <DataTable
+        title="History"
+        data={user?.role === "admin" ? transactions : filteredTransaction}
+        columns={ColumnsHistory}
+      />
     </>
   );
 };
