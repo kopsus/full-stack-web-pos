bbd5febc690178ddb5a59a983ef228106c997c65
kopsus <emonsetio01@gmail.com>
2025-11-28 09:00:37 +0700

    feat : set up metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,22 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "LOEMPIA BOOM",
   description: "Management Kasir Loempia Boom",
+  openGraph: {
+    title: "LOEMPIA BOOM",
+    description: "Management Kasir Loempia Boom",
+    url: "https://loempia-bom.putra-tunggal.my.id",
+    siteName: "LOEMPIA BOOM",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Preview Loempia Boom",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
